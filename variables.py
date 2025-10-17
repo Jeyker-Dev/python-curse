2e854901ca784db0a3f21c8c4fbb2eea29a7aae2
Jeyker <mendozajeyker1178@gmail.com>
2025-10-17 19:46:10 -0400

    Initial commit

---
+++ variables.py
@@ -0,0 +1,11 @@
+from typing import Optional, Any
+
+num: int = 1
+name: str = "Jeyker"
+false: bool = False
+missing: Optional[int] = None
+price: float = 10.99
+
+print("The value of num is:", num)
+print("My name is:", name)
+print("The value of false is:", false)
