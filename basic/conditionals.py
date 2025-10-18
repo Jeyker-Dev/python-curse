beedd6ec8bbbee14389181b5eedfdb377ca700d4
Jeyker <mendozajeyker1178@gmail.com>
2025-10-18 14:23:44 -0400

    Add examples of conditional statements in conditionals.py

---
+++ basic/conditionals.py
@@ -0,0 +1,21 @@
+from typing import Optional, Any
+
+num: int = 10
+num_2: int = 20
+
+# Conditional statements
+if num > 0:
+    print("The number is positive.")
+if num < 11:
+    print("The number is less than 10 but positive.")
+
+
+if num_2 < num:
+    print("num_2 is less than num.")
+elif num_2 > num:
+    print("num_2 is greater than num.")
+
+
+
+
+
